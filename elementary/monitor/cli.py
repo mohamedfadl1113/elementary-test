f7d91e9aa096639b139a2045cf3341fb04f8bbf1
hanalytics-data-service <data-service@data-hanalytics.io>
2024-03-06 14:38:21 +0100

    Removing CLI Modifications

--- elementary/monitor/cli.py
+++ elementary/monitor/cli.py
@@ -604,12 +604,12 @@ def report(
     help="Include additional information at the test results summary message.\nCurrently only --include descriptions is supported.",
 )
 
-@click.option(
-    "--notification-title,",
-    type=str,
-    default=None,
-    help="The title of the notification message.",
-)
+#@click.option(
+#    "--notification-title,",
+#    type=str,
+#    default=None,
+#    help="The title of the notification message.",
+#)
 
 @click.pass_context
 def send_report(
@@ -623,7 +623,7 @@ def send_report(
     slack_token,
     slack_channel_name,
     slack_file_name,
-    notification_title,
+    #notification_title,
     profile_target,
     project_profile_target,
     executions_limit,
@@ -677,7 +677,7 @@ def send_report(
         dbt_quoting=dbt_quoting,
         slack_token=slack_token,
         slack_channel_name=slack_channel_name,
-        notification_title = notification_title,
+        #notification_title = notification_title,
         update_bucket_website=update_bucket_website,
         aws_profile_name=aws_profile_name,
         aws_region_name=aws_region_name,
